9a52e827e9a30efc7931ed7b6b537447489fe06a
Mari329 <mariyappan329@gmail.com>
2021-05-22 14:38:40 +0530

    Till Async Redux

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import './homepage.styles.scss';
+
 import Directory from '../../components/directory/directory.component';
 
+import './homepage.styles.scss';
+
+import { HomePageCointainer } from './homepage.styles'
+
 const HomePage = () => (
-	<div className='homepage'>
+	<HomePageCointainer>
 		<Directory />
-	</div>
+	</HomePageCointainer>
 );
 
 export default HomePage;
