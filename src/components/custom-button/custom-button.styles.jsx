eefb1f434f10cb4e93741073a22312108c898716
Mari329 <mariyappan329@gmail.com>
2021-05-25 15:24:25 +0530

    Before Hooks

--- src/components/custom-button/custom-button.styles.jsx
+++ src/components/custom-button/custom-button.styles.jsx
@@ -48,11 +48,11 @@ export const CustomButtonContainer = styled.button`
 	height: 50px;
 	letter-spacing: 0.5px;
 	line-height: 50px;
-	padding: 0 35px 0 35px;
-	font-size: 15px;
+	padding: 0 20px 0 20px;
+	font-size: 14px;
 	text-transform: uppercase;
 	font-family: 'Open Sans Condensed';
-	font-weight: bolder;
+	//font-weight: bolder;
 	cursor: pointer;
 	display: flex;
 	justify-content: center;
